e221cd02a1fd0fc69e11b165bfce87a653206603
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-17 10:28:46 +0000

    Initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react"
+
+export default function Form(){
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    return(
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            console.log({name:{name}, email:{email}, password:{password}})
+        }}>
+            <div>
+                <label for="name">Name</label>
+                <input id="name" type="text" onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input id="email" type="email" onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input id="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <input type="submit" value="submit"/>
+        </form>
+    )
+}
